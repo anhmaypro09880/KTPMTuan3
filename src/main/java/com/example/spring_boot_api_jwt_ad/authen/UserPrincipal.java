4dd2e82c38ab4159f59aefbd70359a76dc9bc47f
Phan Nguyen Tan Trong <anhmaypro09880@gmail.com>
2022-09-19 05:44:55 +0700

    fix loi kh

---
+++ src/main/java/com/example/spring_boot_api_jwt_ad/authen/UserPrincipal.java
@@ -0,0 +1,81 @@
+package com.example.spring_boot_api_jwt_ad.authen;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+
+
+
+public class UserPrincipal implements UserDetails {
+    private Long userId;
+    private String username;
+    private String password;
+    private Collection authorities;
+    
+
+    public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setAuthorities(Collection authorities) {
+		this.authorities = authorities;
+	}
+
+	@Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
